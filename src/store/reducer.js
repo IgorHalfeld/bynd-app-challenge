a8258a9ecd2d43fd4001a97561f02ac0577930bb
Igor Halfeld <hello@igorluiz.me>
2019-07-08 01:09:12 -0300

    remove logs

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -14,16 +14,6 @@ export default (state, action) => {
     case 'setCurrentUserLocation':
       return { ...state, user: action.payload };
     case 'setDistanceBetweenCoordinates':
-      console.log('action paylod', action.payload);
-      console.log('data', {
-        ...state,
-        distance: getDistanceFromLatLon(
-          action.payload.lat1,
-          action.payload.lng1,
-          action.payload.lat2,
-          action.payload.lng2,
-        ),
-      });
       return {
         ...state,
         distance: getDistanceFromLatLon(
